d8230ae60c64c6633fa27bd26a9fff7f378712e3
ShoheiTakei <st.jwd196@gmail.com>
2024-12-15 19:40:37 +0900

    add stub data

--- src/app.jsx
+++ src/app.jsx
@@ -2,11 +2,26 @@ import Form from './Form'
 import List from './List'
 
 const App = () => {
+  const todos = [
+    {
+      content: '課題をする'
+    },
+    {
+      content: '選択をする'
+    },
+    {
+      content: '電話をする'
+    },
+    {
+      content: '料理をする'
+    }
+  ]
+
   return (
     <>
       <h1>Todo App</h1>
       <Form />
-      <List/>
+      <List todos={todos} />
     </>
   )
 }
