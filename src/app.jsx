7365bdf96f973d5172b76008ea30aa760b5dbe9d
ShoheiTakei <st.jwd196@gmail.com>
2024-12-15 18:35:03 +0900

    update issue 3

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import Form from './form'
 
 const App = () => {
   return (
-    <React.Fragment>
+    <>
       <h1>Todo App</h1>
       <Form/>
-    </React.Fragment>
+    </>
   )
 }
 
